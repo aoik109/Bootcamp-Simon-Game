056def9ed56dbb075b5549b54a42bf3d89ac5143
aoik109 <allison.ko.family@gmail.com>
2023-11-09 10:58:41 -0800

    stray kids program

---
+++ Section 15 - Intermediate JavScript/strayKids.js
@@ -0,0 +1,10 @@
+// Program that asks for the user's name and tells them if they are in Stray Kids or not
+var strayKids = ["chan", "leeknow", "changbin", "han", "felix", "seungmin", "in"];
+
+var name = prompt("What is your name?").toLowerCase();
+
+if (strayKids.includes(name)) {
+    alert("Hi, " + name + ". You are in Stray Kids!");
+} else {
+    alert("Sorry, " + name + ". You are NOT in Stray Kids...");
+}
