b7804e1f41b0cdd368b1b14a4d605e57efe345f6
aoik109 <allison.ko.family@gmail.com>
2023-11-18 13:30:51 -0800

    fixed player 1's dice

--- Section 17 - Boss Level Challenge 1 - The Dicee Game/Dicee+Challenge+-+Starting+Files/Dicee Challenge - Starting Files/index.js
+++ Section 17 - Boss Level Challenge 1 - The Dicee Game/Dicee+Challenge+-+Starting+Files/Dicee Challenge - Starting Files/index.js
@@ -7,15 +7,15 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 // depending on the random number, change the img for player 1 dice
 var player1Dice = document.querySelector(".img1");
 if (randomNumber1 === 1) {
-    player1Dice.setAttribute("src", "./images.dice1");
+    player1Dice.setAttribute("src", "./images/dice1.png");
 } else if (randomNumber1 === 2) {
-    player1Dice.setAttribute("src", "./images.dice2");
+    player1Dice.setAttribute("src", "./images/dice2.png");
 } else if (randomNumber1 === 3) {
-    player1Dice.setAttribute("src", "./images.dice3");
+    player1Dice.setAttribute("src", "./images/dice3.png");
 } else if (randomNumber1 === 4) {
-    player1Dice.setAttribute("src", "./images.dice4");
+    player1Dice.setAttribute("src", "./images/dice4.png");
 } else if (randomNumber1 === 5) {
-    player1Dice.setAttribute("src", "./images.dice5");
+    player1Dice.setAttribute("src", "./images/dice5.png");
 } else {
-    player1Dice.setAttribute("src", "./images.dice6");
+    player1Dice.setAttribute("src", "./images/dice6.png");
 }
