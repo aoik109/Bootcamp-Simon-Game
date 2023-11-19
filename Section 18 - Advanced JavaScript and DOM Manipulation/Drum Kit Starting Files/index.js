9a3cd40360f337a20320534a3cfd5e6593d540c2
aoik109 <allison.ko.family@gmail.com>
2023-11-19 15:33:57 -0800

    edited drum kit

--- Section 18 - Advanced JavaScript and DOM Manipulation/Drum Kit Starting Files/index.js
+++ Section 18 - Advanced JavaScript and DOM Manipulation/Drum Kit Starting Files/index.js
@@ -1,12 +1,22 @@
-/*document.querySelector("button").addEventListener("click", function() {
-    alert("I got clicked!");
-});*/
-
 var buttons = document.querySelectorAll(".drum");
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function() {
         alert("I got clicked!");
     });
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////
+/*document.querySelector("button").addEventListener("click", function() {
+    alert("I got clicked!");
+});*/
+
+
+
+    // document.querySelectorAll(".drum")[i].addEventListener....
 }
 
 // function handleClick() {
