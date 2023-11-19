3056afb98d985789a95b5656238ed29168c536f2
aoik109 <allison.ko.family@gmail.com>
2023-11-19 15:07:41 -0800

    added event listener to all the buttons

--- Section 18 - Advanced JavaScript and DOM Manipulation/Drum Kit Starting Files/index.js
+++ Section 18 - Advanced JavaScript and DOM Manipulation/Drum Kit Starting Files/index.js
@@ -1,5 +1,14 @@
-document.querySelector("button").addEventListener("click", handleClick);
+/*document.querySelector("button").addEventListener("click", function() {
+    alert("I got clicked!");
+});*/
 
-function handleClick() {
-    alert("I got clicked")
+var buttons = document.querySelectorAll(".drum");
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        alert("I got clicked!");
+    });
 }
+
+// function handleClick() {
+//     alert("I got clicked")
+// }
