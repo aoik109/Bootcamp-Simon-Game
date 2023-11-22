0eeadfd540e81a0b025112e4b45397e3df088116
aoik109 <allison.ko.family@gmail.com>
2023-11-22 10:51:17 -0800

    toggle title color when button is clicked

--- Section 19 - jQuery/index.js
+++ Section 19 - jQuery/index.js
@@ -1,10 +1,18 @@
 // $("h1").css("color", "red");
 //separation of concerns -> we don't want javascript to deal with style
-// $("h1").addClass("big-title");
+$("h1").addClass("big-title");
 
-$("button").html("<a href='#'>Click Me</a>");
-$("a").attr("href", "https://jquery.com/");
-$("a").attr("target", "_blank");
+// $("button").html("<a href='#'>Click Me</a>");
+// $("a").attr("href", "https://jquery.com/");
+// $("a").attr("target", "_blank");
+
+$("h1").click(function() {
+    $("h1").css("color", "blue");
+});
+
+$("button").click(function() {
+    $("h1").toggleClass("purple-title");
+})
 
 // check if jQuery is ready -> once ready, the anonomyous function will be called
 // $(document).ready(function() {
