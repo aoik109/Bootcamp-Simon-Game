5e934fd859c9298f7e9ad5d2ea429cee0bdbcc0a
aoik109 <allison.ko.family@gmail.com>
2023-11-22 10:24:15 -0800

    added a class using jQuery

--- Section 19 - jQuery/index.js
+++ Section 19 - jQuery/index.js
@@ -1,4 +1,6 @@
-$("h1").css("color", "red");
+// $("h1").css("color", "red");
+//separation of concerns -> we don't want javascript to deal with style
+$("h1").addClass("big-title");
 
 // check if jQuery is ready -> once ready, the anonomyous function will be called
 // $(document).ready(function() {
