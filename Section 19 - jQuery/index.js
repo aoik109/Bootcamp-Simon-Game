680455b5d5598437ab74c5396dca55fd3748a92f
aoik109 <allison.ko.family@gmail.com>
2023-11-22 10:36:06 -0800

    added jQuery website to the buttons and then made it open in new tab!

--- Section 19 - jQuery/index.js
+++ Section 19 - jQuery/index.js
@@ -1,6 +1,10 @@
 // $("h1").css("color", "red");
 //separation of concerns -> we don't want javascript to deal with style
-$("h1").addClass("big-title");
+// $("h1").addClass("big-title");
+
+$("button").html("<a href='#'>Click Me</a>");
+$("a").attr("href", "https://jquery.com/");
+$("a").attr("target", "_blank");
 
 // check if jQuery is ready -> once ready, the anonomyous function will be called
 // $(document).ready(function() {
