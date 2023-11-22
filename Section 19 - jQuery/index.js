2f4b1580950276d45b8cc05110de20f348a3853d
aoik109 <allison.ko.family@gmail.com>
2023-11-21 16:30:50 -0800

    ready

--- Section 19 - jQuery/index.js
+++ Section 19 - jQuery/index.js
@@ -1,1 +1,4 @@
-$("h1").css("color", "red");
+// check if jQuery is ready -> once ready, the anonomyous function will be called
+$(document).ready(function() {
+    $("h1").css("color", "red");
+});
