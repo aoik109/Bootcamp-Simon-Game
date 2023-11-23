823604be48b3fd9b0302e59077c8aa0f5d1d5958
aoik109 <allison.ko.family@gmail.com>
2023-11-23 15:01:55 -0800

    playGame, pickAndPlay

--- Section 20 - Boss Level Simon Game/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/index.js
+++ Section 20 - Boss Level Simon Game/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/index.js
@@ -1,11 +1,51 @@
 var correctOrder = []; // holds the correct sequence of buttons
-var levelNum = 1;
+var levelNum = 1; // current level number
+var tempArray = []; // holds the current level's input
+
+var buttons = $(".btn");
 
 // press any key to start
 $(document).keypress(function(){
     playGame();
 });
 
+// plays the sound
 function playGame() {
     $("h1").text("Level " + levelNum);
+
+    if (levelNum === 1) {
+        pickAndPlayButton();
+    }
+}
+
+// picks random button, adds to correctOrder array and plays sound
+function pickAndPlayButton() {
+    var randomIndex = Math.floor(Math.random() * buttons.length);
+    var randomButtonId = buttons[randomIndex].getAttribute("id")''
+    correctOrder.push(randomButtonId);
+    playSound(randomButtonId);
+}
+
+function playSound(randomButtonId) {
+    var audioFile = 
+    switch (randomButtonId) {
+        case "green":
+            
+            break;
+
+        case "red":
+            
+            break;
+
+        case "yellow":
+            
+            break;
+
+        case "blue":
+            
+            break;
+    
+        default:
+            break;
+    }
 }
